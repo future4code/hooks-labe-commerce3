458f14bcbc900c5384560d56f7aa61097a18d933
Leandro Pereira de Jesus <le0406@hotmail.com>
2022-05-07 13:41:00 -0300

    começando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './components/Home'
+import Produtos from './components/Produtos'
+import Filtro from './components/Filtro'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Home/>    
+     <Produtos/>
+     <Filtro/>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Produtos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Produtos extends React.Component{
+  render(){
+    return(
+      <>
+      Hi my name is Sandra
+      </>
+    )
+  }
+}
+export default Produtos;
