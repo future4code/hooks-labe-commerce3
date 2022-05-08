75d93a364551e616e0394732beac71eaf49a8e5d
dev-Diogo <diogohbomfim@gmail.com>
2022-05-08 14:21:55 -0300

    Mudei a disposição dos elementos no site para ficar mais fácil de visualizar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import GlobalStyles from "./GlobalStyles";
 import Home from './components/Home'
 import Carrinho from './components/Carrinho'
 import Filtro from './components/Filtro'
+import styled from 'styled-components';
 
+const Body = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+`
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <GlobalStyles/>
-        <Home/>    
-        <Carrinho/>
-        <Filtro/>
+        <Body>
+          <GlobalStyles/>
+          <Home/>    
+          <Carrinho/>
+          <Filtro/>
+        </Body>
       </>
     )
   }
